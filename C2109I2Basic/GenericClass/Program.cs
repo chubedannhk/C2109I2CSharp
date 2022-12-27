c01005026b5fcd0aa8fe4dd872f20dfd227ec1ef
Nguyen Hoang Khai <nguyenhoangkhai2003tgl@gmail.com>
2022-12-27 14:17:49 +0700

    generic and method syntax, query syxtax

---
+++ C2109I2Basic/GenericClass/Program.cs
@@ -0,0 +1,21 @@
+﻿using GenericClass;
+
+
+// generic thuong viet tac bang cac ky hieu T,U,V
+ClassK<int> cl = new();
+cl.Field = 10;
+cl.Show();
+ClassK<string> cl1= new();
+cl1.Field = "nguyenhoangakha";
+cl1.Show();
+
+
+ClassUV<bool,string> cl3= new();
+cl3.Field1= true;
+cl3.Field2 = "abc";
+cl3.Show();
+
+ClassUV<int, double> cl4 = new();
+cl4.Field1 = 100;
+cl4.Field2 = 105.12;
+cl4.Show();
--- C2109I2Basic/ListAndLinq/Program.cs
+++ C2109I2Basic/ListAndLinq/Program.cs
@@ -33,8 +33,8 @@ List<Student> students = new()
 // sql => select from where group by having order by
 // C# => from where groud by having order by ... select
 // linq co 2 dang:
-  // style 1 : query systax => dua theo cu phap cua sql , de hoc , de dung
-  // style 2 : method syntax => dua theo lambda => kho hoc, kho dung, nhung cuc nhanh
+// style 1 : query systax => dua theo cu phap cua sql , de hoc , de dung
+// style 2 : method syntax => dua theo lambda => kho hoc, kho dung, nhung cuc nhanh
 
 // hien thi toan thong tin sinhvien trong list , voi dieu kien id  sv phai lon hon hai
 
@@ -61,17 +61,99 @@ List<Student> students = new()
 //}
 
 // style 2: cua linq: methods syntax => chay nhanh hon thang style 1
-var t = students.Where(stu => stu.Id > 2);
-foreach (var i in t)
-{
-    Console.WriteLine(i);
-}
+//var t = students.Where(stu => stu.Id > 2);
+//foreach (var i in t)
+//{
+//    Console.WriteLine(i);
+//}
 
 // rut gon
-foreach ( var i in students.Where(stu => stu.Id > 2))
-{
-    Console.WriteLine(i);
-}
+//foreach ( var i in students.Where(stu => stu.Id > 2))
+//{
+//    Console.WriteLine(i);
+//}
 
 // ke hop voi foreach cua list
-students.Where(stu => stu.Id > 2).ToList().ForEach(stu => Console.WriteLine());
+//students.Where(stu => stu.Id > 2).ToList().ForEach(stu => Console.WriteLine());
+
+//=================================================================================
+
+
+//var t = from stu in students select stu;
+//// viet tuong tu thay tu var bang IEnumerable
+//IEnumerable<Student> i = from stu in students select stu;
+
+// voi linq to object 
+// khi su dung query syntax hay method syntax thi tra ve khong phai la list, hay array => ma tra ve mot thang la IEnumerable ( con cua ienumerator duyet qua colection: list, array, dictionary, hashmap,...) dung de duyet qua linq to object. 
+//students.ForEach(Console.WriteLine);
+//// t.ToList() = studetns 
+//t.ToList().ForEach(Console.WriteLine);
+
+
+//==============
+// cai nay la cau lenh
+//foreach(var stu in students)
+//{
+
+
+//    if (stu.Id >= 2)
+//    {
+//        Console.WriteLine(stu);
+//    }
+//}
+// cai nay la phuong thuc su dung lamd nen chay nhanh hon, khong phai method la chay nhanh hon, nhung method co lamd thi auto chay nhan :))
+//students.ForEach(student =>
+//{
+//    if (student.Id >= 2)
+//    {
+//        Console.WriteLine(student);
+//    }
+//}
+//);
+
+// linq 
+// method syntax
+//students.Where(stu => stu.Id > 2)
+//    .ToList()
+//    .ForEach(Console.WriteLine);
+// query syntax
+
+//var t = from stu in students
+//        where stu.Id >2
+//        select stu;
+//t.ToList().ForEach(Console.WriteLine);
+
+// lay thuoc tinh su sinh vien nhu cau select lay cac cot
+//query syntax
+var t = from stu in students
+        where stu.Id > 2
+        select new
+        {
+            Info = $"{stu.Id}:{stu.Name}",
+            stu.Gender,
+            Birthday = stu.Dob 
+        };
+t.ToList().ForEach(t => Console.WriteLine(t.Birthday));
+
+// method syntax
+students.Where(stu => stu.Id >= 2)
+        .Select(stu => new
+        {
+            Info = $"{stu.Id}:{stu.Name}",
+            stu.Gender
+        })
+        .ToList()
+        .ForEach(Console.WriteLine);
+
+// sap xep trat tu: 
+// mac dinh la tang dan
+students.Where(stu => stu.Id <= 3)
+        .OrderBy(stu=> stu.Id)
+        .ThenBy(stu=>stu.Name)
+        .ToList() .ForEach(Console.WriteLine);
+
+// sap xep giam dan
+students.Where(stu => stu.Id <= 3)
+        .OrderByDescending(stu => stu.Id)
+        .ThenByDescending(stu => stu.Name)
+        .ToList().ForEach(Console.WriteLine);
